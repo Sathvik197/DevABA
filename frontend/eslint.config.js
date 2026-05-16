966b70588e1950c04691f46a4ccedbd5a93370a7
Sathvik197 <vvce23ise0255@vvce.ac.in>
2026-05-16 11:09:52 +0530

    new changes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,16 +1,18 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
   {
-    plugins: { react },
     languageOptions: {
       parserOptions: { ecmaFeatures: { jsx: true } },
-      globals: { window: true, document: true, console: true, fetch: true, confirm: true, setTimeout: true, clearTimeout: true }
+      globals: {
+        window: true, document: true, console: true,
+        fetch: true, confirm: true, setTimeout: true,
+        clearTimeout: true
+      }
     },
     rules: {
-      "react/react-in-jsx-scope": "off"
+      "no-unused-vars": "warn"
     }
   }
 ];
